b77100319e7c9f610494b86f7505c64e686cb4bf
StackMobile <mylovewoman1990@hotmail.com>
2017-01-20 19:41:10 +0100

    Link page added

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -9,6 +9,8 @@ router.use('/forgot_password', require('./login/forgot_password.js'));
 /* -- Home Page --*/
 
 router.use('/home', require('./home/home.js'));
+router.use('/newlink', require('./home/newlink.js'));
+router.use('/links', require('./home/links.js'));
 
 
 module.exports = router;
